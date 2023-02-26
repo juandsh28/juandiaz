5bc25688a8eb74a8decb2df0d2495c26054886ae
Juan <juanjodiazsh28@gmail.com>
2023-02-25 23:14:32 -0500

    Agregado del deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abortar por errores
+set -e
+
+# build
+npm run build
+
+# navegar al directorio de salida de la compilación
+cd dist
+
+# si está implementando en un dominio personalizado
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# si está implementando en https: // <USERNAME> .github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# si está implementando en https: // <USERNAME> .github.io / <REPO>
+# git push -f git@github.com:<juandsh28>/<juandiaz>.git main:gh-pages
